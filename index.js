aa316e6898fd9ceb6fde1bdfb4045e7c71dd8373
Yamin Miah <yaminmiah264@gmail.com>
2021-06-15 17:58:38 +0600

    read data from server

--- index.js
+++ index.js
@@ -1,38 +1,49 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const MongoClient = require("mongodb").MongoClient;
 const bodyParser = require("body-parser");
-require('dotenv').config()
+const MongoClient = require("mongodb").MongoClient;
+require('dotenv').config();
 
-const port = process.env.PORT || 5050;
+const port = process.env.PORT || 5055;
 
 app.use(cors());
 app.use(bodyParser.json());
 
 
 app.get("/", (req, res) => {
-    res.send("Hello World!");
+    res.send("Hello World!?");
 });
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.psabv.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
-
-const client = new MongoClient(uri, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-});
-client.connect((err) => {
-    console.log('connection err', err);
+console.log(uri);
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+    console.log('Connection err', err);
     const productCollection = client.db("cycleshop").collection("product");
-
+    
     app.post('/addProduct', (req, res) => {
         const newProduct = req.body;
         console.log('adding new product: ', newProduct)
+        productCollection.insertOne(newProduct)
+        .then(result => {
+            console.log('inserted count:', result.insertedCount)
+            res.send(result.insertedCount > 0 )
+        })
     })
+    app.get('/product', (req, res) => {
+        productCollection.find()
+            .toArray((err, items) => {
+                console.log("🚀 ~ file: index.js ~ line 37 ~ .toArray ~ items", items)
+                res.send(items)
+            
+        })
+    })
+
+
+  // perform actions on the collection object
     // client.close();
 });
-
-
 
 
 app.listen(port, () => {
