7f492281e0388c7fe9187e5b34e75096e2cd7f3c
Yamin Miah <yaminmiah264@gmail.com>
2021-06-15 18:18:40 +0600

    added data

--- index.js
+++ index.js
@@ -16,25 +16,21 @@ app.get("/", (req, res) => {
 });
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.psabv.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
-console.log(uri);
+
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
-    console.log('Connection err', err);
     const productCollection = client.db("cycleshop").collection("product");
     
     app.post('/addProduct', (req, res) => {
         const newProduct = req.body;
-        console.log('adding new product: ', newProduct)
         productCollection.insertOne(newProduct)
         .then(result => {
-            console.log('inserted count:', result.insertedCount)
             res.send(result.insertedCount > 0 )
         })
     })
     app.get('/product', (req, res) => {
         productCollection.find()
             .toArray((err, items) => {
-                console.log("🚀 ~ file: index.js ~ line 37 ~ .toArray ~ items", items)
                 res.send(items)
             
         })
